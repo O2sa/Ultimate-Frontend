6380bca643fa4c9df1ebd5da05668f67db9babeb
o2sa <o2sama1@outlook.sa>
2023-05-14 22:36:50 +0300

    Lesson 15

--- stege/js/main.js
+++ stege/js/main.js
@@ -3,4 +3,11 @@ $(function () {
   $(".toggle-sidebar").on("click", function () {
     $(".content-area,.sidebar").toggleClass("no-sidebar");
   });
+  //sidebar toggle submenu
+  $(".toggle-submenu").on("click",function(){
+    $(this).find(".fa-angle-right").toggleClass("down");
+    $(this)
+    .next(".child-links").slideToggle();
+  });
 });
+ 

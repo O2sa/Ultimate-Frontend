9e8e5eb41a2917f0705da10f00393016f7207df3
o2sa <o2sama1@outlook.sa>
2023-05-17 19:21:48 +0300

    Lesson 18

--- stege/js/main.js
+++ stege/js/main.js
@@ -4,10 +4,46 @@ $(function () {
     $(".content-area,.sidebar").toggleClass("no-sidebar");
   });
   //sidebar toggle submenu
-  $(".toggle-submenu").on("click",function(){
+  $(".toggle-submenu").on("click", function () {
     $(this).find(".fa-angle-right").toggleClass("down");
-    $(this)
-    .next(".child-links").slideToggle();
+    $(this).next(".child-links").slideToggle();
+  });
+  //toggle fullscreen
+  $(".toggle-fullscreen").on("click", function () {
+    $(this).toggleClass("full-screen");
+    if ($(this).hasClass("full-screen")) {
+      openFullscreen();
+    } else {
+      closeFullscreen();
+    }
   });
 });
- 
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE11 */
+    document.msExitFullscreen();
+  }
+}

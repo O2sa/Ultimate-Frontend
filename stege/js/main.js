f3637f6246aad0297d4687f90ba7d084231811a0
o2sa <o2sama1@outlook.sa>
2023-05-19 20:17:36 +0300

    Lesson 24

--- stege/js/main.js
+++ stege/js/main.js
@@ -21,6 +21,18 @@ $(function () {
     $(this).find("i").toggleClass("fa-spin");
     $(this).parent().toggleClass("hide-setting");
   });
+
+  //switch colors themes
+  var themesClasses=[];
+  $(".color-options li").each(function(){
+    themesClasses.push($(this).data("theme"));
+    console.log(themesClasses);
+  });
+  $(".color-options li").on("click", function(){
+    $(this).addClass("active").siblings().removeClass("active");
+    $("body").removeClass(themesClasses.join(" "))
+    .addClass($(this).data("theme"));
+  })
 });
 
 /* Get the documentElement (<html>) to display the page in fullscreen */
